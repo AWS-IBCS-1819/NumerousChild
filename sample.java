07cab18b860f78fe913a18cc71d1f17cfc7b501b
Anna Salam <anna_salam@m013150.aw.org>
2018-09-07 13:48:48 -0700

    updated notes

--- sample.java
+++ sample.java
@@ -1,8 +1,24 @@
-//sample.java by Salam
+/*
+sample.java by Anna S
+Java-basics notes 9/7/18
+*/
+import java.util.*;
 
-public class sample {
-  public static void main(String[] args) {
+public class sample { //purple words are key words
+  //"public" denotes to all other something that something something about using it in other stuff idk
+  //"class" is a key word that identifies that everything following is part of the class
+  //"{}" define what the ___ (class/method) is
+
+
+  public static void main(String[] args) { //type "main" and it will complete the main method for you #shortcuts
+  //"static" is a keyword -- generally we want methods to be static and variables to only be static if they aren't going ot change
+  //"void" refers to what the return values are. if there are no return values then it is void
+
     System.out.println("just a sample");
+    //"system" refers to actual computer
+    //"out" is the output
+    //"println" is a method to print ____ on a new line
+    //don't forget ";" after lines to separate them -- ";" = a line break in your code
   }
 
 }
