2908b20501b7afafac896edd4997b9f9fdf4a161
Anna Salam <anna_salam@m013150.aw.org>
2018-09-12 13:00:12 -0700

    first draft

---
+++ AboutMe.java
@@ -0,0 +1,29 @@
+// AboutMe.java project by Anna Salam
+// partner = Rashida Francis
+
+import java.util.*;
+
+public class AboutMe{
+
+  public String color; //as in favorite color      This could change
+  public static String hand; //as in right/left handed
+  public static String subject; //as in favorite subject
+  public String interest; //Things you find interesting but others may not    This could change so it's mutable
+  public static String birthplace; //as in where you were born
+  public static Integer siblings; //how many siblings you have <--this could be an integer but it's easier to keep things consistent
+  public static String time; //as in are you a night or day person? <--this could be a boolean but it's easier to keep things consistent
+
+  public AboutMe() {
+    color = "blue";
+    hand = "right handed";
+    subject = "history";
+    interest = "reading";
+    birthplace = "Jamaica";
+    siblings = 2;
+    time = "night";
+
+  }
+
+
+
+}
