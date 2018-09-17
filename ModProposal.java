bd7326a1c1f0fb77755ae787fd3f147c8dcea328
Anna Salam <anna_salam@m013150.aw.org>
2018-09-17 14:26:19 -0700

    still an error but class ended

--- ModProposal.java
+++ ModProposal.java
@@ -19,6 +19,8 @@ public class ModProposal {
       while (sc.hasNext()){
 
         myStr.append(sc.nextLine() + "\n");
+
+        //count++; //this says everytime we move on for each loop, add one to count. basically to count every while loop
       }
     }
     catch (FileNotFoundException e){
@@ -28,5 +30,19 @@ public class ModProposal {
     System.out.println(myStr);
 
     //go to string builder api and check out index to see what you can do with this text file
+    int num = 0;
+    for (int i = 0; i < myStr.capacity() - 4; i++){
+      if (myStr.substring(i, i + 4).equals("baby")){
+        num++;
+      }
+      else if (myStr.substring(i, i + 4).equals("END END")){
+        break;
+      }
+    }
+    System.out.println("Baby is mentioned " + Integer.toString(num) + " times in A Modest Proposal.");
+
+//still an error but class ended
   }
+
+
 }
