d1c635dfa9b24efa84a9f783a54f4a9eedf799e3
Anna Salam <anna_salam@m013150.local>
2018-09-20 20:05:18 -0700

    matching up

--- Gradebook.java
+++ Gradebook.java
@@ -3,9 +3,34 @@ import java.io.*;
 
 public class Gradebook{
 
+  ArrayList<Double[]> list = new ArrayList<>();
+
+  public void readFile(String filename){
+    try{
+      FileReader fr = new FileReader(filename);
+      Bufferedreader bufferedReader = new bufferedReader(fileReader); //BufferedReader goes through a file and checks all the chars/lines/arrays
+
+      while(line = bufferedReader.readLine() != null) {
+        line.convertInt();
+      }
+      bufferedReader.close();
+      
+    }
+    catch(FileNotFoundException ex){
+      System.out.println("Unable to open file " + filenName + " .");
+    }
+    catch(IOException ex){
+      System.out.println("Error reading file " + fileName + " .");
+    }
+  }
+
+  public double convertInt(String s){
+
+  }
+
   public static void main(String[] args) {
 
   }
 
-  
+
 }
