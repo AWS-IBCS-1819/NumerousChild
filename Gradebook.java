84663e36bbbd76bb9ab73ad7dd8dd33e43befd9b
Anna Salam <anna_salam@m013150.local>
2018-09-23 11:16:29 -0700

    made skeleton

--- Gradebook.java
+++ Gradebook.java
@@ -3,127 +3,83 @@ import java.io.*;
 
 public class Gradebook{
 
-  /*public void readFile(fileName){
+ public void readFile(String filename) {
 
-    StringBuilder mysb = new StringBuilder();
+   StringBuilder myStr = new StringBuilder();
+     try {
+       File randtxt = new File("grades.txt");
+       Scanner sc = new Scanner(randtxt);
+       while (sc.hasNext()){
+         myStr.append(sc.nextLine() + "\n");
+       }
+   }
+     catch (FileNotFoundException e){
+         e.printStackTrace();
+             }
 
-    try {
-      File gradetxt = new File(fileName);
-      Scanner sc = new Scanner(gradetxt);
+     System.out.println(myStr);
 
-      while (sc.hasNext()){
+ }
 
-        mysb.append(sc.nextLine() + "\n");
-      }
+ public void findMean(String mean){//finds average of grades
+
+ }
+
+ public void findSpread(String spread){//finds number of As/Bs/Cs/Ds/Fs
+
+ }
+
+ public void highestGrade(String highest){
+   System.out.println("Directly comparing grades does good to no one except the person with the highest grade. It is one of many parts of the flawed educational system.");
+ }
+
+ public void lowestGrade(String lowest){
+   System.out.println("Directly comparing grades does good to no one except the person with the highest grade. It is one of many parts of the flawed educational system.");
+ }
+
+ public void roster(String roseter){//finds name of all students
+
+ }
+
+ public void subjectTeacher(String subTeach){//finds name of subject and Teacher
+
+ }
 
 
-      for (int i = 0; i < mysb.length(); i = i + 1){
-        if (mysb.substring(i).equals("%")){
-          codePointCount(int i-4, int i){//can't work because it needs to be its own method
-            Double.parseDouble(i);
-          }
-        }
-      }
 
 
-    }
+ public static void main(String[] args) {
 
-    catch (FileNotFoundException e){
-      e.printStackTrace();
-    }
+   Gradebook gb = new Gradebook();
+   gb.readFile("grades.txt");
 
-  }
+   System.out.println("Choose which you would like to see: \n1. Mean Grades \n2. Grade Spread \n3. Highest Grade \n4. Lowest Grade \n5. Class Subject and Teacher \n6. Student Roster");
 
-  public int indexOf(String str){
+   Scanner scan = new Scanner(Syemtem.in);
+   String scan = scan.next();
+   if(scan.equals("1")){//mean of grades
+     gb.findMean("grades.txt");
+   }
+   if(scan.equals("2")){
+     gb.findSpread("grades.txt");
+   }
+   if(scan.equals("3")){
+     gb.highestGrade("grades.txt");
+   }
+   if(scan.equals("4")){
+     gb.lowestGrade("grades.txt");
+   }
+   if(scan.equals("5")){
+     gb.subjectTeacher("grades.txt");
+   }
+   if(scan.equals("6")){
+     gb.roster("grades.txt");
+   }
+   else{
+     System.out.println("Sorry. We don't understand that request.");
+   }
 
-  }*/
 
-/*  ArrayList<Double[]> list = new ArrayList<>();
-
-  public void readFile(String filename){
-    try{
-      FileReader fr = new FileReader(filename);
-      Bufferedreader bufferedReader = new bufferedReader(fileReader); //BufferedReader goes through a file and checks all the chars/lines/arrays
-
-      while(line = bufferedReader.readLine() != null) {
-        line.convertInt();
-      }
-      bufferedReader.close();
-
-    }
-    catch(FileNotFoundException ex){
-      ex.printStackTrace();
-    }
-    catch(IOException ex){
-      System.out.println("Error reading file " + fileName + " .");
-    }
-  }
-
-  public double convertInt(String s){
-
-  }*/
-
-  public static void main(String[] args) {
-    StringBuilder myStr = new StringBuilder();
-    try {
-      File randtxt = new File("grades.txt");
-      Scanner sc = new Scanner(randtxt);
-      while (sc.hasNext()){
-        myStr.append(sc.nextLine() + "\n");
-      }
-    }
-    catch (FileNotFoundException e){
-      e.printStackTrace();
-    }
-
-    System.out.println(myStr);
-
-    /*StringBuilder mysb = new StringBuilder();
-    try {
-      File gradetxt = new File(fileName);
-      Scanner sc = new Scanner(gradetxt);
-      while (sc.hasNext()){
-        mysb.append(sc.nextLine() + "\n");
-      }
-      for (int i = 0; i < mysb.length(); i = i + 1){
-        if (mysb.substring(i).equals("%")){
-          codePointCount(int i-4, int i){//can't work because it needs to be its own method
-            Double.parseDouble(i);
-          }
-        }
-      }
-    }
-    catch (FileNotFoundException e){
-      e.printStackTrace();
-    }
-
-    System.out.println("Choose which you would like to see: \n1. Mean Grades \n2. Grade Spread \n3. Highest Grade \n4. Lowest Grade \n5. Class Subject and Teacher \n6. Student Roster");
-    Scanner scan = new Scanner(Syemtem.in);
-    String scan = scan.next();
-    if(scan.equals("1")){//mean of grades
-
-    }
-    if(scan.equals("2")){
-
-    }
-    if(scan.equals("3")){
-
-    }
-    if(scan.equals("4")){
-
-    }
-    if(scan.equals("5")){
-
-    }
-    if(scan.equals("6")){
-
-    }
-    else{
-      System.out.println("Sorry. We don't understand that request.");
-    }*/
-  }
-
-//method for converting string to double
-//Double.parseDouble("the number you want to convert");
+ }
 
 }
