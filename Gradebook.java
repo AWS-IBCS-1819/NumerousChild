a38034b695ce752667f0a5fa949fb54a0343d4c3
Anna Salam <anna_salam@m013150.aw.org>
2018-09-26 13:40:34 -0700

    figured out how to print subject and teacher

--- Gradebook.java
+++ Gradebook.java
@@ -1,54 +1,96 @@
+
 import java.util.*;
 import java.io.*;
 
 public class Gradebook{
-
- public void readFile(String filename) {
-
-   StringBuilder myStr = new StringBuilder();
-     try {
-       File randtxt = new File("grades.txt");
-       Scanner sc = new Scanner(randtxt);
-       while (sc.hasNext()){
-         myStr.append(sc.nextLine() + "\n");
-       }
-   }
-     catch (FileNotFoundException e){
-         e.printStackTrace();
-             }
-
-     System.out.println(myStr);
-
- }
-
- public void findMean(String mean){//finds average of grades
-
- }
-
- public void findSpread(String spread){//finds number of As/Bs/Cs/Ds/Fs
-
- }
-
- public void highestGrade(String highest){
-   System.out.println("Directly comparing grades does good to no one except the person with the highest grade. It is one of many parts of the flawed educational system.");
- }
-
- public void lowestGrade(String lowest){
-   System.out.println("Directly comparing grades does good to no one except the person with the highest grade. It is one of many parts of the flawed educational system.");
- }
-
- public void roster(String roseter){//finds name of all students
-
- }
-
- public void subjectTeacher(String subTeach){//finds name of subject and Teacher
-
- }
+/*
+  public void readFile(String filename) {
 
 
+    StringBuilder myStr = new StringBuilder();
+      try {
+        File randtxt = new File("grades.txt");
+        Scanner sc = new Scanner(randtxt);
+        while (sc.hasNext()){
+          myStr.append(sc.nextLine() + "\n");
+        }
+    }
+      catch (FileNotFoundException e){
+          e.printStackTrace();
+              }
+
+      System.out.println(myStr);
+
+  }
+
+  public void findMean(String mean){//finds average of grades
+
+  }
+
+  public void findSpread(String spread){//finds number of As/Bs/Cs/Ds/Fs
+
+  }
+
+  public void highestGrade(String highest){
+    System.out.println("Directly comparing grades does good to no one except the person with the highest grade. It is one of many parts of the flawed educational system.");
+  }
+
+  public void lowestGrade(String lowest){
+    System.out.println("Directly comparing grades does good to no one except the person with the highest grade. It is one of many parts of the flawed educational system.");
+  }
+
+  public void roster(String roseter){//finds name of all students
+
+  }
+
+  public void subjectTeacher(String filename){//finds name of subject and Teacher
+
+    String line1 = null;
+
+    try {
+      File randtxt = new File("grades.txt");
+      Scanner sc = new Scanner(randtxt);
+      while (sc.hasNext()){
+        line1 = sc.nextLine();
+      }
+  }
+    catch (FileNotFoundException e){
+        e.printStackTrace();
+            }
+    System.out.println(line1);
+
+  }*/
 
 
- public static void main(String[] args) {
+  public static void main(String[] args) {
+
+    Gradebook gb = new Gradebook();
+
+    StringBuilder myStr = new StringBuilder();
+      try {
+        File randtxt = new File("grades.txt");
+        Scanner sc = new Scanner(randtxt);
+
+        /*myStr.append(sc.next());//code for printing out subject
+        myStr.append(sc.next());
+        myStr.append(sc.next());
+        myStr.append(sc.next());*/
+        myStr.append(sc.nextLine());
+        System.out.println(myStr);
+
+        while (sc.hasNext()){
+          myStr.append(sc.nextLine() + "\n");
+        }
+    }
+      catch (FileNotFoundException e){
+          e.printStackTrace();
+              }
+
+      //System.out.println(myStr);
+
+}
+
+ /*public static void main(String[] args) {
 
    Gradebook gb = new Gradebook();
    gb.readFile("grades.txt");
@@ -80,6 +122,6 @@ public class Gradebook{
    }
 
 
- }
+ }*/
 
 }
