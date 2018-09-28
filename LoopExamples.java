60aaf0fb1232ab2751dd42ce30644c62cf787e0a
Anna Salam <anna_salam@m013150.aw.org>
2018-09-28 15:30:24 -0700

    doesn't compile but oh well

--- LoopExamples.java
+++ LoopExamples.java
@@ -10,15 +10,15 @@ public class LoopExamples{
 
 //loop for #2
     int sum = 0;
-    System.out.println("Input a value for y");
-    Scanner twoA = new Scanner(System.in);
+    System.out.println("Input a value for y");//requests a y value
+    Scanner twoA = new Scanner(System.in);//scans input for a y value since it is meant to be unknown
     int y = twoA.nextInt();
     System.out.println("Input a value for x");
     Scanner twoB = new Scanner(Sytem.in);
     int x = twoB.nextInt();
-    for(int i = x; i < y; i++){
+    for(int i = x; i < y; i++){//i starts at x value; the loop continues until i is less than y; adds 1 to i until then
       sum = sum + i;
-    }
+    }//for this problem if you meant the sum of all numbers between x and y INCLUDING y then I would do i =< y instead of i < y
     return sum;
 
 //loop for #3
@@ -27,9 +27,9 @@ public class LoopExamples{
     int stringIndex = three.nextInt();
     int number = 0;
     int amountOfnum = 0;
-    for(int i = 0; i < stringIndex + 1; i++){
+    for(int i = 0; i < stringIndex + 1; i++){//first loop to go through all the numbers in the string
       i = number;
-      for(int d = 0; i < stringIndex + 1; d++){
+      for(int d = 0; d < stringIndex + 1; d++){//second loop to compare i to each number in the loop. I need something in here to not compare i to i.
         if(d = i){
           amountOfnum = amountOfnum + 1;
         }
