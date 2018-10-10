df8eca2b18dc25308676d4026630df381e456f69
Anna Salam <anna_salam@m013150.aw.org>
2018-10-10 13:52:02 -0700

    shuffled cards

--- ArrayList/PokerHand.java
+++ ArrayList/PokerHand.java
@@ -18,9 +18,10 @@ inser a card @ specific index
     Random r = new Random(); //<-- this is to create a new object; constructor method
     int[] ar = r.ints(6300, 0, 52).distinct().toArray();//can't just print array
     for (int i = 0; i < ar.length; i ++){//loop is to help print contents of array
-      System.out.println(ar[i]);
+      //System.out.println(ar[i]);
+      System.out.println(d.cards.get(ar[i]));
     }
-    System.out.println("\n" + ar.length);
+    //System.out.println("\n" + ar.length);
 
     //System.out.println(d.cards);
   }
