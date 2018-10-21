6c6a81377210d5a85f083480eca84f36d1a08273
Anna Salam <anna_salam@m013150.local>
2018-10-20 19:59:07 -0700

    no errors but doesn't work

--- cardGames/Blackjack.java
+++ cardGames/Blackjack.java
@@ -8,7 +8,9 @@ public class Blackjack {
   //need to shuffle cards and reset them.
 
   public Blackjack(){
-
+    d = new Deck();
+    userHand = new ArrayList<Card>();
+    compHand = new ArrayList<Card>();
   }
 
   /*public ArrayList<String> giveHand(ArrayList<Card> hand){//I think this needs to be a while loop
@@ -35,15 +37,31 @@ DON'T NEED THIS METHOD. JUST ADD TWO CARDS TO USERHAND AND COMPHAND SEPARATELY I
   public static void main(String[] args) {
 
     Blackjack b = new Blackjack();//don't forget blackjack object
+    b.d.reset();
     b.d.shuffle();
 
     System.out.println("Welcome to Blackjack.");
 
-    System.out.println("Your hand: \n" + b.giveHand(b.userHand)); //print out cards dealt
+    for (int i = 0; i <2; i++){
+      Card c = b.d.drawCard();
+      b.userHand.add(c);
+    }
+    System.out.println("Your hand: \n" + b.userHand); //print out cards dealt
     System.out.println("Value: \n" + b.calculateHand(b.userHand));//print integer value from calculateHand method
 
-    /*System.out.println("Your opponent is showing a " + giveHand(compHand));
+    for (int n = 0; n <2; n++){
+      Card c = b.d.drawCard();
+      b.userHand.add(c);
+    }
+    System.out.println("Your opponent is showing a " + b.userHand);
 
+    /*if (b.d.calculateHand(b.compHand) <= 16){
+      b.d.drawCard(b.userHand);
+    }
+    else{
+      System.out.println(b.compHand);//print compHand
+    }*/
+/*
     System.out.println("What would you like to do? \n1. Hit\n2. Stay\n3. Quit");
     Scanner input = new Scanner(System.in);
     String choice = input.next();
