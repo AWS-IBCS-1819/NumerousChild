1735e1bbc790134a80b56f3f47d8aa445d5165ba
Anna Salam <anna_salam@m013150.local>
2018-10-22 19:42:25 -0700

    last work for the week

--- cardGames/Blackjack.java
+++ cardGames/Blackjack.java
@@ -22,6 +22,15 @@ public class Blackjack {
     return total;
   }
 
+  public String cardNames(ArrayList<Card> haha){
+    String cName = " ";
+    for (int i = 0; i <= haha.size(); i++ ){
+      haha.get(i).getName();
+      cName = cName + ", " + haha.get(i).getName();
+    }
+    return cName;
+  }
+
   public static void main(String[] args) {
 
     Blackjack b = new Blackjack();//don't forget blackjack object
@@ -34,41 +43,55 @@ public class Blackjack {
       Card c = b.d.drawCard();
       b.userHand.add(c);
     }
-    System.out.println("Your hand: \n" + b.userHand + "\nValue: \n" + b.calculateHand(b.userHand)); //print out cards dealt and integer value from calculateHand method
+    System.out.println("Your hand:\n" + b.userHand + "\nValue: \n" + b.calculateHand(b.userHand)); //print out cards dealt and integer value from calculateHand method
 
     for (int n = 0; n <2; n++){//adds 2 cards to compHand
       Card c = b.d.drawCard();
-      b.userHand.add(c);
+      b.compHand.add(c);
     }
-    System.out.println("Your opponent is showing a " + b.userHand);
+    System.out.println("Your opponent is showing a " + b.compHand.get(1));
 
-    System.out.println("What would you like to do? \n1. Hit\n2. Stay\n3. Quit");
+    Boolean runProgram = true;
     Scanner input = new Scanner(System.in);
+
+    while(runProgram){
     String choice = input.next();
+    System.out.println("What would you like to do? \n1. Hit\n2. Stay\n3. Quit");
     if(choice == "1"){
       b.d.drawCard(b.userHand);
-      System.out.println("Your hand: \n" + b.userHand + "\nYour value:\n" + b.calculateHand(b.userHand));
+      System.out.println("Your hand: \n" + b.cardNames(b.userHand) + "\nYour value:\n" + b.calculateHand(b.userHand));
+
+
     }
-    else if (choice == "2"){
-      //do what the user needs to do
-      if (b.d.calculateHand(b.compHand) <= 16){
+
+    else if (choice == "2"){//if we stay, dealer logic starts
+      //do what the comp needs to do
+      System.out.println("Dealer's hand:\n" + b.cardNames(b.compHand.get(1)) + "\nValue:" + b.calculateHand(b.compHand.get(1)));
+      if (b.calculateHand(b.compHand) <= 16){
         b.d.drawCard(b.userHand);
       }
       else{
-        System.out.println(b.compHand);//print compHand
+        System.out.println("Dealer's hand:\n" + b.compHand + "\nValue:" + b.calculateHand(b.compHand));//print compHand
+        if(b.calculateHand(b.compHand) > 21){
+          System.out.println("Bust!\nYou win!");
+          break;//should go to "do you want to play again?"
+        }
+        else if(b.calculateHand(b.compHand) < b.calculateHand(b.userHand)){
+          System.out.println("You win!");
+          break;//should go to "do you want to play again?"
+        }
+        else{
+          System.out.println("You lose.");
+          break;//should go to "do you want to play again?"
+        }
       }
     }
+
     else{
       break;
-    }
+      }
+  }
 
-//if statement for compHand stuff
-    if (b.d.calculateHand(b.compHand) <= 16){
-      b.d.drawCard(b.userHand);
-    }
-    else{
-      System.out.println(b.compHand);//print compHand
-    }
 
   }
 
