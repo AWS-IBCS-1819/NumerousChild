bcd6d9e3caec0bd2a93108c83348adbd23ce727f
Anna Salam <anna_salam@m013150.aw.org>
2019-02-08 13:05:49 -0800

    update w/ class notes

--- Beeper.java
+++ Beeper.java
@@ -34,7 +34,9 @@ package events;
 /*
  * Beeper.java requires no other files.
 
-COMMENT THIS AS HOMEWORK 2/6
+COMMENT THIS AS HOMEWORK 2/6 (lower case notes)
+
+//CAPATALIZED NOTES ARE CLASS NOTES
  */
 
 import java.awt.*;
@@ -49,31 +51,31 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;//importing a million libraries for various methods used throughout
 
 public class Beeper extends JPanel//subclass = Beeper. super class = JPanel
-                    implements ActionListener {//ActionListener is implemented so we can use its super cool functions
-    JButton button;//creating a button (instance variable) in Beeper.
+                    implements ActionListener {//ActionListener is implemented so we can use its super cool functions //USES CLASS: ACTION LISTENER
+    JButton button;//creating a button (instance variable) in Beeper. //INSTANCE VARIABLE
 
-    public Beeper() {
-        super(new BorderLayout());//using BorderLayout method from JPanel superclass
+    public Beeper() {// CONSTRUCTOR METHOD
+        super(new BorderLayout());//IS CONSTRUCTOR OF SUPER CLASS
         button = new JButton("Click Me");//assigning the button variable to say "Click Me"
         button.setPreferredSize(new Dimension(200, 80));//this is just setting the size of the button
-        add(button, BorderLayout.CENTER);//This adds the button to the window in the center
-        button.addActionListener(this);//adds ActionListener to the button (so user can interact with button and things can happen)
+        add(button, BorderLayout.CENTER);//This adds the button to the window in the center //ADD: ADDING ___ TO THE FRAME (WHICH HAPPENS TO BE CALLED "BEEPER")
+        button.addActionListener(this);//adds ActionListener to the button (so user can interact with button and things can happen) //WE ADD THE ACTION LISTENER TO WHATEVER COMPONENT WE WANT IT TO BE ASSIGNED TO (IN THIS CASE, BUTTON)
     }
 
     public void actionPerformed(ActionEvent e) {//when user clicks button this (below) happens
-        Toolkit.getDefaultToolkit().beep();//method "beep" happens from Toolkit library
+        Toolkit.getDefaultToolkit().beep();//method "beep" happens from Toolkit library //MAKES A SOUND "BEEP"
     }
 
 
     public static void main(String[] args) {
-      JFrame frame = new JFrame("Beeper");//creates the frame/window and names it "Beeper"
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//this makes it so when you close the window, you close the whole program too
+      JFrame frame = new JFrame("Beeper");//creates the frame/window and names it "Beeper" //USES JFRAME
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//this makes it so when you close the window, you close the whole program too //ONE OF THOSE THINGS A FRAME COULD DO. IT HAS A DEFAULT CLOSE OPERATION RATHER THAN SOMETHING ELSE
 
       JComponent newContentPane = new Beeper();//creating new Beeper object
       newContentPane.setOpaque(true);//makes the background of the window opaque
       frame.setContentPane(newContentPane);
 
       frame.pack();
-      frame.setVisible(true);//makes the frame/window visible
+      frame.setVisible(true);//makes the frame/window visible //THIS IS ESSENTIAL IF YOU WANT THE FRAME TO APPEAR
     }
 }
