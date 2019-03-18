31cf29324b72aa1ea3f9b6d764aabf9ffca7f9a8
Anna Salam <anna_salam@m013150.aw.org>
2019-03-18 15:40:15 -0700

    done with coding

--- IA/Tringo.java
+++ IA/Tringo.java
@@ -30,6 +30,9 @@ public class Tringo extends Frame implements WindowListener,ActionListener {
   Button b20;
   Button start;
   Button next;
+  TextArea color = new TextArea(10,25);
+  TextArea color2 = new TextArea(10,25);
+
 
   //ArrayList<String> questions;
   //ArrayList<Integer> sheet;
@@ -52,70 +55,78 @@ public class Tringo extends Frame implements WindowListener,ActionListener {
 
         start = new Button("Start");
         add(start);
-        start.addActionListener(new ActionListener() {
-          public void actionPerformed(ActionEvent e) {
-            text.setText(cor.questions.get(0));
-            next.setVisible(false);
-            start.setVisible(false);
-          }
-        });
         next = new Button("Next Question");
         add(next);
-        next.addActionListener(new ActionListener(){
-          public void actionPerformed(ActionEvent e){
-            j=j+1;
-            text.setText(cor.questions.get(j));
-            next.setVisible(false);
-          }
-        });
 
 
 
         b = new Button("cos(60)");//adding each button to the GUI in specific order, each are labeled with answers to questions
         add(b);
+        //b.setVisible(false);
         b2 = new Button("tan(30)");
         add(b2);
+        //b2.setVisible(false);
         b3 = new Button("90º");
         add(b3);
+        //b3.setVisible(false);
         b4 = new Button("0º");
         add(b4);
+        //b4.setVisible(false);
         b5 = new Button("sin(0)");
         add(b5);
+        //b5.setVisible(false);
         b6 = new Button("sin(60)");
         add(b6);
+        //b6.setVisible(false);
         b7 = new Button("45º");
         add(b7);
+        //b7.setVisible(false);
         b8 = new Button("sin(90)");
         add(b8);
+        //b8.setVisible(false);
         b9 = new Button("sin(30)");
         add(b9);
+        //b9.setVisible(false);
         b10 = new Button("cos(45)");
         add(b10);
+        //b10.setVisible(false);
         b11 = new Button("tan(45)");
         add(b11);
+        //b11.setVisible(false);
         b12 = new Button("cos(90)");
         add(b12);
+        //b12.setVisible(false);
         b13 = new Button("cos(30)");
         add(b13);
+        //b13.setVisible(false);
         b14 = new Button("tan(60)");
         add(b14);
+        //b14.setVisible(false);
         b15 = new Button("sec(60)");
         add(b15);
+        //b15.setVisible(false);
         b16 = new Button("30º");
         add(b16);
+        //b16.setVisible(false);
         b17 = new Button("sin(45)");
         add(b17);
+        //b17.setVisible(false);
         b18 = new Button("csc(60)");
         add(b18);
+        //b18.setVisible(false);
         b19 = new Button("60º");
         add(b19);
+        //b19.setVisible(false);
         b20 = new Button("cos(0)");
         add(b20);
+        //b20.setVisible(false);
 
         //text.setText(cor.questions.get(0));//automatically prints first question in TextField
 
         add(which);
         add(oops);
+        add(color);
+        add(color2);
 
 
             /*questions.add("π/2");//b3 -0
@@ -178,6 +189,7 @@ public class Tringo extends Frame implements WindowListener,ActionListener {
                 b6.addActionListener(new ActionListener() {
                   public void actionPerformed(ActionEvent e) {
                     quest = 1;
+                    //b13.setVisible(false);
                   }
                 });
                 b6.addActionListener(this);
@@ -227,6 +239,8 @@ public class Tringo extends Frame implements WindowListener,ActionListener {
                 b13.addActionListener(new ActionListener() {
                   public void actionPerformed(ActionEvent e) {
                     quest = 1;
+                    //make it wait a moment
+                    //b6.setVisible(false);
                   }
                 });
                 b13.addActionListener(this);
@@ -280,6 +294,48 @@ public class Tringo extends Frame implements WindowListener,ActionListener {
                 });
                 b20.addActionListener(this);
 
+        //which.setText("CLICK START");
+        color.setText("The goal of this game is to\nanswer questions until all \nbuttons have disappeared or\nare colored.");
+        color2.setText("Click START when you're ready.");
+        start.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+              text.setText(cor.questions.get(0));
+              next.setVisible(false);
+              start.setVisible(false);
+              color2.setText("");
+              color.setText("");
+              /*b.setVisible(true);
+              b2.setVisible(true);
+              b3.setVisible(true);
+              b4.setVisible(true);
+              b5.setVisible(true);
+              b6.setVisible(true);
+              b7.setVisible(true);
+              b8.setVisible(true);
+              b9.setVisible(true);
+              b10.setVisible(true);
+              b11.setVisible(true);
+              b12.setVisible(true);
+              b13.setVisible(true);
+              b14.setVisible(true);
+              b15.setVisible(true);
+              b16.setVisible(true);
+              b17.setVisible(true);
+              b18.setVisible(true);
+              b19.setVisible(true);
+              b20.setVisible(true);*/
+          }
+        });
+
+        next.addActionListener(new ActionListener(){
+          public void actionPerformed(ActionEvent e){
+            j=j+1;
+            text.setText(cor.questions.get(j));
+            next.setVisible(false);
+            which.setText("");
+          }
+        });
+
         }
 
       public static void main(String[] args) {
@@ -294,40 +350,82 @@ public class Tringo extends Frame implements WindowListener,ActionListener {
         Component c = (Component)e.getSource();*/
       //  for(int i=0; i<13; i++){
           //text.setText(cor.questions.get(i));
-          which.setText("i is " + i);
+          //which.setText("i is " + i);
           Component c = (Component)e.getSource();
 
           if (quest == cor.sheet.get(i)){//i = index for questions loop
             //button disappears
-            c.setVisible(false);
+            //c.setVisible(false);
             next.setVisible(true);
             i=i+1;
-            which.setText("i is " + i);//worked for the first question and transition over to second question but didn't work for the second answer of second question.
+            if(i>j){
+              which.setText("click Next");
+            }
+            //which.setText("i is " + i);//worked for the first question and transition over to second question but didn't work for the second answer of second question.
 
 
-            /*if(quest == 1){
+            if(quest == 1){
               //question remains until one other correct button is clicked
-              c.setVisible(false);
+              /*b6.setVisible(false);
+              b13.setVisible(false);*/
+              b6.setBackground(Color.blue);
+              b13.setBackground(Color.blue);
+              color.setText("blue:\nsin(60)\ncos(30)");
             }
             else if(quest == 4){
               //question remains until one other correct button is clicked
-              c.setVisible(false);
+              /*b10.setVisible(false);
+              b17.setVisible(false);*/
+              b10.setBackground(Color.cyan);
+              b17.setBackground(Color.cyan);
+              color.setText("blue:\nsin(60)\ncos(30)\n\ncyan:\ncos(45)\nsin(45)");
+              color2.setText("yellow:\n0º\nsin(0)\ncos(90)");
+
             }
             else if(quest == 5){
               //question remains until one other correct button is clicked
-              c.setVisible(false);
+              /*b.setVisible(false);
+              b9.setVisible(false);*/
+              b.setBackground(Color.green);
+              b9.setBackground(Color.green);
+              color.setText("blue:\nsin(60)\ncos(30)\n\ncyan:\ncos(45)\nsin(45)\n\ngreen:\ncos(60)\nsin(30)");
+              color2.setText("yellow:\n0º\nsin(0)\ncos(90)");
+
             }
             else if(quest == 3){
                 //question remains until two other correct buttons are clicked
-                c.setVisible(false);
+                /*b4.setVisible(false);
+                b5.setVisible(false);
+                b12.setVisible(false);*/
+                b4.setBackground(Color.yellow);
+                b5.setBackground(Color.yellow);
+                b12.setBackground(Color.yellow);
+                color.setText("blue:\nsin(60)\ncos(30)");
+                color2.setText("yellow:\n0º\nsin(0)\ncos(90)");
+
             }
             else if(quest == 8){
               //question remains until two other correct buttons are clicked
+              /*b8.setVisible(false);
+              b11.setVisible(false);
+              b20.setVisible(false);*/
+              b8.setBackground(Color.red);
+              b11.setBackground(Color.red);
+              b20.setBackground(Color.red);
+              color.setText("blue:\nsin(60)\ncos(30)\n\ncyan:\ncos(45)\nsin(45)\n\ngreen:\ncos(60)\nsin(30)");
+              color2.setText("yellow:\n0º\nsin(0)\ncos(90)\n\nred:\nsin(90)\nan(45)\ncos(0)");
+
+            }
+            else if(quest == 12){
               c.setVisible(false);
+              which.setText("FINISHED!");
+              color.setText("Congratulations!\nWrite down how many Wrong\nTries you have and give \nit to your teacher.");
+              color2.setText("When you're done,\nclose this window to \nexit the game.");
             }
             else{
+              c.setVisible(false);
               next.setVisible(true);
-            }*/
+            }
           }
 
           else{
